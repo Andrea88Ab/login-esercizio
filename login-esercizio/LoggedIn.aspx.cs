df90387c9e9783273124a9f8cb9244a41d331ad3
Andrea88Ab <andrea88ab@gmail.com>
2024-02-13 14:48:01 +0100

    Aggiungere i file di progetto.

---
+++ login-esercizio/LoggedIn.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web;
+
+namespace login_esercizio
+{
+    public partial class LoggedIn : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                HttpCookie userCookie = Request.Cookies["UserInfo"];
+                if (userCookie != null)
+                {
+                    lblUsername.Text = userCookie["Username"];
+                }
+                else
+                {
+                    Response.Redirect("Login.aspx");
+                }
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            HttpCookie userCookie = new HttpCookie("UserInfo")
+            {
+                Expires = DateTime.Now.AddDays(-1) // Imposta la data di scadenza nel passato per cancellarlo
+            };
+            Response.Cookies.Add(userCookie);
+
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
---
+++ login-esercizio/Login.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web;
+
+namespace login_esercizio
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            HttpCookie userCookie = new HttpCookie("UserInfo");
+            userCookie["Username"] = txtUsername.Text;
+            userCookie.Expires = DateTime.Now.AddHours(1); // Il cookie scade dopo 1 ora
+            Response.Cookies.Add(userCookie);
+
+            Response.Redirect("LoggedIn.aspx");
+        }
+    }
+}
